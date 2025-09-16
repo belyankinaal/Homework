8f3a3b0da2b952a4afb550a186b336468bdf5d0a
belyankina anastasia <anastas.belya@ifellow.ru>
2025-09-16 19:37:40 +0700

    Работа готова

---
+++ src/main/java/Autotesting/Mazda.java
@@ -0,0 +1,44 @@
+package Autotesting;
+
+public class Mazda extends Car {
+    public String series; //марка
+    public int topRating; // оценка по 10 бальной
+    public int miliage;
+
+    public Mazda (int year,
+                  boolean isAutomatic,
+                  String color,
+                  String country,
+                  int price,
+                  double engine,
+                  String series,
+                  int topRating,
+                  int miliage){
+        super(year, isAutomatic, color, country, price, engine);
+        this.series = series;
+        this.topRating = topRating;
+        this.miliage = miliage;
+    }
+
+    // Методы гетеры
+    public String getSeries(){
+        return series;
+    }
+
+    public int getTopRating() {
+        return topRating;
+    }
+    public  int getMiliage(){
+        return miliage;
+    }
+    //Методы сетеры
+    public void setSeries (String series){
+        this.series = series;
+    }
+    public void setTopRating (int topRating) {
+        this.topRating=topRating;
+    }
+    public void setMiliage (int price){
+        this.miliage=miliage;
+    }
+}
