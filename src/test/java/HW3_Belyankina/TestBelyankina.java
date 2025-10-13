887c865301ff42c07cbb3d890065326266215a84
belyankina anastasia <anastas.belya@ifellow.ru>
2025-10-14 02:01:11 +0700

    steps

--- src/test/java/HW3_Belyankina/TestBelyankina.java
+++ src/test/java/HW3_Belyankina/TestBelyankina.java
@@ -1,31 +1,24 @@
 package HW3_Belyankina;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pages.NewTaskPage;
-import pages.ProjectPage;
-import pages.VisualEditorPage;
+import pages.*;
 import steps.AuthSteps;
-import steps.IssueSteps;
 import steps.NavigationSteps;
 import steps.WorkFlowSteps;
-import util.CustomProperties;
 import util.WebHooks;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestBelyankina extends WebHooks {
-    @BeforeAll
-    static void beforeAll() {
-        CustomProperties.loadProperties();
-    }
 
     private final AuthSteps authSteps = new AuthSteps();
     private final NavigationSteps navigationSteps = new NavigationSteps();
     private final WorkFlowSteps workFlowSteps = new WorkFlowSteps();
-    private final IssueSteps issueSteps = new IssueSteps();
 
+    private final TaskListPage taskListPage = new TaskListPage();
+    private final NewTaskPage newTaskPage = new NewTaskPage();
+    private final TaskSearchPage taskSearchPage = new TaskSearchPage();
     private final ProjectPage projectPage = new ProjectPage();
 
     private void loginAndOpenTestProject() {
@@ -48,13 +41,13 @@ public class TestBelyankina extends WebHooks {
     @Test
     @DisplayName("3. Проверка увеличения количества задач")
     void issueCountTest() {
-        String issueSummary = CustomProperties.getProperty("issue.summary");
-
         loginAndOpenTestProject();
-        int countBefore = issueSteps.getIssueCount();
-        issueSteps.createIssue(issueSummary);
-        int countAfter = issueSteps.getIssueCount();
-
+        taskListPage.clickViewAllIssues();
+        int countBefore = taskListPage.extractNumberFromText(taskListPage.getIssuesCountText());
+        taskListPage.clickCreateIssue();
+        newTaskPage.enterSummary("A1").submitIssue();
+        taskListPage.navigateBackToIssues().refreshIssuesList();
+        int countAfter = taskListPage.extractNumberFromText(taskListPage.getIssuesCountText());
         assertTrue(countAfter > countBefore,
                 "Количество задач должно увеличиться минимум на 1. Было: " + countBefore + ", стало: " + countAfter);
     }
@@ -62,12 +55,14 @@ public class TestBelyankina extends WebHooks {
     @Test
     @DisplayName("4. Проверка созданной задачи и статуса")
     void checkTaskTest() {
-        String issueSummary = CustomProperties.getProperty("issue.summary");
-
         loginAndOpenTestProject();
-        issueSteps.createIssue(issueSummary);
-        issueSteps.searchAndOpenTask("TestSeleniumATHomework");
-
+        taskListPage.clickViewAllIssues()
+                .clickCreateIssue();
+        newTaskPage.enterSummary("A1").submitIssue();
+        taskListPage.navigateBackToIssues().refreshIssuesList();
+        taskSearchPage.clickViewAllTasks()
+                .searchForTask("TestSeleniumATHomework")
+                .openTask();
         projectPage.verifyTaskStatus("Сделать")
                 .verifyFixVersion("Version 2.0");
     }
@@ -75,29 +70,31 @@ public class TestBelyankina extends WebHooks {
     @Test
     @DisplayName("5. Создание и прохождение дефекта")
     void createBugTest() {
-        String issueSummary = CustomProperties.getProperty("issue.summary");
-
         loginAndOpenTestProject();
-        int countBefore = issueSteps.getIssueCount();
-        issueSteps.createIssue(issueSummary);
-        int countAfter = issueSteps.getIssueCount();
-
+        taskListPage.clickViewAllIssues();
+        int countBefore = taskListPage.extractNumberFromText(taskListPage.getIssuesCountText());
+        taskListPage.clickCreateIssue();
+        newTaskPage.enterSummary("A1").submitIssue();
+        taskListPage.navigateBackToIssues().refreshIssuesList();
+        int countAfter = taskListPage.extractNumberFromText(taskListPage.getIssuesCountText());
         assertTrue(countAfter > countBefore,
                 "Количество задач должно увеличиться минимум на 1. Было: " + countBefore + ", стало: " + countAfter);
 
-        issueSteps.searchAndOpenTask("TestSeleniumATHomework");
+        taskSearchPage.clickViewAllTasks()
+                .searchForTask("TestSeleniumATHomework")
+                .openTask();
         projectPage.verifyTaskStatus("Сделать")
                 .verifyFixVersion("Version 2.0");
 
-        issueSteps.createIssue(issueSummary);
-
+        taskListPage.clickCreateIssue();
+        newTaskPage.enterSummary("A1");
         new VisualEditorPage(0).ensureVisualEditorSelected().setContent("Дефект");
         projectPage.selectFixVersionByText("Version 2.0");
         new VisualEditorPage(1).setContent("DEV");
         projectPage.enterIssueLinkAndPressEnter("Test-207008");
         projectPage.enterSprintAndPressEnter("Доска Спринт 1");
         projectPage.selectSeverityByValue("10100");
-        new NewTaskPage().clickSubmitAndWaitForSuccessAndOpenIssue();
+        newTaskPage.clickSubmitAndWaitForSuccessAndOpenIssue();
 
         workFlowSteps.completeWorkflow("В процессе", "Исполнено", "Подтверждено");
     }
