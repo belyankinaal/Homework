b4c561fd15c8ffda1c69532d1219a8c69f022417
belyankina anastasia <anastas.belya@ifellow.ru>
2025-10-26 22:51:37 +0700

    Настройка тестов кукумбер

---
+++ src/main/java/ifellow/belyankina/steps/MortyCharacterSteps.java
@@ -0,0 +1,55 @@
+package ifellow.belyankina.steps;
+
+import ifellow.belyankina.dto.Character;
+import ifellow.belyankina.dto.Episode;
+import ifellow.belyankina.service.RickAndMortyService;
+import ifellow.belyankina.util.Specification;
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+
+import java.util.logging.Logger;
+
+public class MortyCharacterSteps {
+
+    private static final Logger log = Logger.getLogger(MortyCharacterSteps.class.getName());
+    private RickAndMortyService service;
+    private Character morty;
+    private Episode episode;
+
+    @Дано("^установлена спецификация Rick and Morty API$")
+    public void setupSpecification() {
+        RestAssured.requestSpecification = Specification.forRickAndMorty();
+        service = new RickAndMortyService();
+        log.info("Спецификация Rick and Morty установлена");
+    }
+
+    @Когда("^выполняется поиск персонажа \"([^\"]*)\"$")
+    public void findCharacterByName(String name) {
+        morty = service.getFirstCharacterByName(name);
+        attachJson("Morty JSON", morty.toString());
+    }
+
+    @И("^находится последний эпизод с этим персонажем$")
+    public void findLastEpisodeOfCharacter() {
+        episode = service.getLastEpisodeOfCharacter(morty);
+        attachJson("Episode JSON", episode.toString());
+    }
+
+    @Step("{name}")
+    private void attachJson(String name, String json) {
+        Allure.addAttachment(name, "application/json", json, ".json");
+    }
+
+    // Геттеры для использования в следующих шагах
+    public Character getMorty() {
+        return morty;
+    }
+
+    public Episode getEpisode() {
+        return episode;
+    }
+}
--- src/main/java/ifellow/belyankina/steps/RickAndMortySteps.java
+++ src/main/java/ifellow/belyankina/steps/RickAndMortySteps.java
@@ -4,43 +4,20 @@ import ifellow.belyankina.assertions.RickAndMortyAssertions;
 import ifellow.belyankina.dto.Character;
 import ifellow.belyankina.dto.Episode;
 import ifellow.belyankina.service.RickAndMortyService;
-import ifellow.belyankina.util.Specification;
-import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.И;
-import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.Тогда;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
-import io.restassured.RestAssured;
 
 import java.util.logging.Logger;
 
 public class RickAndMortySteps {
 
     private static final Logger log = Logger.getLogger(RickAndMortySteps.class.getName());
-    private RickAndMortyService service;
-    private Character morty;
-    private Episode episode;
+    private RickAndMortyService service = new RickAndMortyService();
     private Character character;
-
-    @Дано("^установлена спецификация Rick and Morty API$")
-    public void setupSpecification() {
-        RestAssured.requestSpecification = Specification.forRickAndMorty();
-        service = new RickAndMortyService();
-        log.info("Спецификация Rick and Morty установлена");
-    }
-
-    @Когда("^выполняется поиск персонажа \"([^\"]*)\"$")
-    public void findCharacterByName(String name) {
-        morty = service.getFirstCharacterByName(name);
-        attachJson("Morty JSON", morty.toString());
-    }
-
-    @И("^находится последний эпизод с этим персонажем$")
-    public void findLastEpisodeOfCharacter() {
-        episode = service.getLastEpisodeOfCharacter(morty);
-        attachJson("Episode JSON", episode.toString());
-    }
+    private Episode episode;
+    private Character morty;
 
     @И("^находится последний персонаж из этого эпизода$")
     public void findLastCharacterOfEpisode() {
@@ -57,5 +34,13 @@ public class RickAndMortySteps {
     @Step("{name}")
     private void attachJson(String name, String json) {
         Allure.addAttachment(name, "application/json", json, ".json");
+    }
+
+    public void setMorty(Character morty) {
+        this.morty = morty;
+    }
+
+    public void setEpisode(Episode episode) {
+        this.episode = episode;
     }
 }
