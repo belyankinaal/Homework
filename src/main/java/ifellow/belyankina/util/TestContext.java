b026ae47bcc67ba8e6958edb80af041c41011c77
belyankina anastasia <anastas.belya@ifellow.ru>
2025-10-27 01:28:14 +0700

    Настройка тестов кукумбер

---
+++ src/main/java/ifellow/belyankina/util/TestContext.java
@@ -0,0 +1,13 @@
+package ifellow.belyankina.util;
+
+public class TestContext {
+    private static String token;
+
+    public static String getToken() {
+        return token;
+    }
+
+    public static void setToken(String token) {
+        TestContext.token = token;
+    }
+}
