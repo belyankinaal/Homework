4ba2348c4429a2266433c9cd674174ce4e76ed99
belyankina anastasia <anastas.belya@ifellow.ru>
2025-10-26 22:45:28 +0700

    Настройка тестов кукумбер

--- src/main/java/ifellow/belyankina/steps/RickAndMortySteps.java
+++ src/main/java/ifellow/belyankina/steps/RickAndMortySteps.java
@@ -9,16 +9,12 @@ import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.Тогда;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
-import io.qameta.allure.Story;
 import io.restassured.RestAssured;
 
 import java.util.logging.Logger;
 
-@Epic("API тесты")
-@Feature("Rick and Morty API")
 public class RickAndMortySteps {
 
     private static final Logger log = Logger.getLogger(RickAndMortySteps.class.getName());
@@ -28,7 +24,6 @@ public class RickAndMortySteps {
     private Character character;
 
     @Дано("^установлена спецификация Rick and Morty API$")
-    @Step("Установка спецификации Rick and Morty API")
     public void setupSpecification() {
         RestAssured.requestSpecification = Specification.forRickAndMorty();
         service = new RickAndMortyService();
@@ -36,55 +31,31 @@ public class RickAndMortySteps {
     }
 
     @Когда("^выполняется поиск персонажа \"([^\"]*)\"$")
-    @Story("Поиск персонажа")
     public void findCharacterByName(String name) {
-        searchCharacter(name);
-    }
-
-    @И("^находится последний эпизод с этим персонажем$")
-    @Story("Поиск последнего эпизода")
-    public void findLastEpisodeOfCharacter() {
-        findLastEpisode();
-    }
-
-    @И("^находится последний персонаж из этого эпизода$")
-    @Story("Поиск последнего персонажа эпизода")
-    public void findLastCharacterOfEpisode() {
-        findLastCharacter();
-    }
-
-    @Тогда("^выполняется сравнение найденных данных персонажей и эпизода$")
-    @Story("Сравнение данных персонажей и эпизода")
-    public void compareData() {
-        performComparison();
-    }
-
-    @Step("Поиск персонажа по имени: {name}")
-    private void searchCharacter(String name) {
         morty = service.getFirstCharacterByName(name);
         attachJson("Morty JSON", morty.toString());
     }
 
-    @Step("Поиск последнего эпизода с персонажем")
-    private void findLastEpisode() {
+    @И("^находится последний эпизод с этим персонажем$")
+    public void findLastEpisodeOfCharacter() {
         episode = service.getLastEpisodeOfCharacter(morty);
         attachJson("Episode JSON", episode.toString());
     }
 
-    @Step("Поиск последнего персонажа из эпизода")
-    private void findLastCharacter() {
+    @И("^находится последний персонаж из этого эпизода$")
+    public void findLastCharacterOfEpisode() {
         character = service.getLastCharacterOfEpisode(episode);
         attachJson("Last Character JSON", character.toString());
     }
 
-    @Step("Сравнение найденных данных персонажей и эпизода")
-    private void performComparison() {
+    @Тогда("^выполняется сравнение найденных данных персонажей и эпизода$")
+    public void compareData() {
         RickAndMortyAssertions.assertFullComparison(morty, character, episode, log);
         log.info("Сравнение завершено успешно");
     }
 
     @Step("{name}")
     private void attachJson(String name, String json) {
-        io.qameta.allure.Allure.addAttachment(name, "application/json", json, ".json");
+        Allure.addAttachment(name, "application/json", json, ".json");
     }
 }
